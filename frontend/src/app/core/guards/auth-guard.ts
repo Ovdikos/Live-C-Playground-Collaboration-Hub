2ec8c440df12631225add4ee6a11367961472633
Oleksandr Ovdiienko <oleksandr.ovdik@gmail.com>
2026-05-11 18:41:35 +0300

    Entry 2.2.2 done. Created auth guard

---
+++ frontend/src/app/core/guards/auth-guard.ts
@@ -0,0 +1,17 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+//TODO admin guard in the same way with authService.currentUserValue?.isAdmin === true
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.getToken()) {
+    return true;
+  }
+
+  router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+  return false;
+};
